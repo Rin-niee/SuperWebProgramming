7e94a32e20f795dfe3d891a518af1adfb7e30ebc
Rin <125102825+Rin-niee@users.noreply.github.com>
2025-01-28 07:19:40 +0000

    Create celery.py

---
+++ car/car/celery.py
@@ -0,0 +1,21 @@
+import os
+from celery import Celery
+from celery.schedules import crontab
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'car.settings')
+
+app = Celery('car')
+
+app.config_from_object('django.conf:settings', namespace = 'CELERY')
+app.autodiscover_tasks()
+
+app.conf.beat_schedule={
+    'fetch-vk-clips-every-24-hours':{
+        'task': 'carapp.tasks.VK_clips',
+        'schedule': crontab(minute =  0, hour = 0),
+    },
+    'fetch-yandex-every-24-hours':{
+        'task': 'carapp.tasks.Yandex',
+        'schedule': crontab(minute =  0, hour = 0),
+    },
+}
